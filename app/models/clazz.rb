1051a36405422ec8bc5a58afb82c384e638ec85c
Amiruddin Nagri <anagri@thoughtworks.com>
2010-10-21 20:28:59 +0530

    [Amir] rails scaffold, first migrations and models

---
+++ app/models/clazz.rb
@@ -0,0 +1,2 @@
+class Clazz < ActiveRecord::Base
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+School::Application.config.secret_token = 'f6fea2a9e9a931619b964b1c68cd4b63737b6719056e3ddc1bf95756edc3d71084dabf8f8a41dac013b4acdae122056c8a143131fa6c6f20e4e1a0e7b00c64ef'
